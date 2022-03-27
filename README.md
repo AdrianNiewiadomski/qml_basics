8f2491f85b387ad99cf46a1a2456b6999ea1cdc7
AdrianNiewiadomski <kontakt@adrianniewiadomski.pl>
2022-03-27 19:59:34 +0200

    first files added.

---
+++ README.md
@@ -0,0 +1,2 @@
+mkvirtualenv qml_basics --python="/usr/bin/python3.9"
+pip install -r requirements.txt
---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+from PySide6.QtWidgets import QApplication
+from PySide6.QtQuick import QQuickView
+
+if __name__ == "__main__":
+    app = QApplication()
+    view = QQuickView()
+
+    view.setSource("1_hello_world.qml")
+
+    view.show()
+    sys.exit(app.exec())
